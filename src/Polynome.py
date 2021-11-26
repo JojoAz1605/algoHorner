64133e802d3dd107da5a82107736e03e26021b3a
JojoAz1605 <joris.masson2002@gmail.com>
2021-11-26 12:42:04 +0100

    Fini la plupart du programme Il reste à standardiser l'écriture à chaque itération

--- src/Polynome.py
+++ src/Polynome.py
@@ -7,8 +7,16 @@ class Polynome:
         self.coeffs = ()
         self.puissances = ()
         self.racineEvidente = 0
+        self.polynomeFactorise = ''
 
         self.horner()
+
+    def resetVal(self):
+        self.monomes = ()
+        self.coeffs = ()
+        self.puissances = ()
+        self.racineEvidente = 0
+
 
     def trouveMonomes(self):
         """Cherche les monômes de l'équation"""
@@ -51,7 +59,7 @@ class Polynome:
                 if monome[i] == "x":
                     try:
                         if monome[i + 1] == "^":
-                            self.puissances += monome[i + 2],
+                            self.puissances += int(monome[i + 2]),
                         else:
                             self.puissances += 1,
                     except IndexError:
@@ -69,7 +77,7 @@ class Polynome:
                     equation += str(self.coeffs[i]) + 'x^' + str(self.puissances[i])
             except IndexError:
                 equation += str(self.coeffs[i])
-        for i in range(-100, 100):
+        for i in range(0, 100):
             equa = equation.replace('x', "*(" + str(i) + ')').replace('^', '**')
             resEqua = eval(equa)
             if resEqua == 0:
@@ -77,21 +85,77 @@ class Polynome:
                 break
         print("La racine évidente trouvée est: ", self.racineEvidente)
 
+    def resolutionSecondDegre(self):
+        try:
+            a = self.coeffs[0]
+        except IndexError:
+            a = 0
+        try:
+            b = self.coeffs[1]
+        except IndexError:
+            b = 0
+        try:
+            c = self.coeffs[2]
+        except IndexError:
+            c = 0
+        delta = ((b**2) - (4*a*c))
+
+        print("Delta vaut: ", delta)
+
+        if delta == 0:
+            x0 = -b/(2 * a)
+            self.polynomeFactorise += str(a) + '(x' + str(-x0) + ')'
+        elif delta < 0:
+            self.polynomeFactorise += '(' + self.equation + ')'
+        else:
+            x1 = (-b - delta ** 0.5) / (2 * a)
+            x2 = (-b + delta ** 0.5) / (2 * a)
+            self.polynomeFactorise += str(a) + '(x+' + str(-x1) + ')' + '(x+' + str(-x2) + ')'
+
+    def coeffsToEqua(self, coeffs):
+        res = ''
+        for i in range(len(coeffs)):
+            try:
+                if coeffs[i] < 0:
+                    if coeffs[i] != 0:
+                        if int(self.puissances[i])-1 != 0:
+                            res += str(coeffs[i]) + 'x^' + str(int(self.puissances[i])-1)
+                        else:
+                            res += str(coeffs[i])
+                else:
+                    if coeffs[i] != 0:
+                        if int(self.puissances[i])-1 != 0:
+                            res += '+' + str(coeffs[i]) + 'x^' + str(int(self.puissances[i])-1)
+                        else:
+                            res += '+' + str(coeffs[i])
+            except IndexError:
+                pass
+        self.equation = res
+
     def horner(self):
-        print("L'équation est: ", self.equation)
-        self.trouveMonomes()
-        self.trouvePuissances()
-        self.trouveCoeffs()  # TODO mettre sous forme standard avant ça
+        while True:
+            print("L'équation est: ", self.equation)
+            self.resetVal()
+            self.trouveMonomes()
+            self.trouvePuissances()
+            self.trouveCoeffs()  # TODO mettre sous forme standard avant ça
+            if max(self.puissances) == 2:
+                break
 
-        self.trouveRacineEvidente()
+            self.trouveRacineEvidente()
 
-        res = ()
-        for i in range(len(self.coeffs)):
-            coeff = self.coeffs[i]
-            if i == 0:
-                res += coeff,
-            else:
-                mult = self.racineEvidente * res[-1]
-                add = mult + coeff
-                res += add,
-        print("Après un passage par le tableau, on trouve ces coefficients là: ", res)
+            res = ()
+            for i in range(len(self.coeffs)):
+                coeff = self.coeffs[i]
+                if i == 0:
+                    res += coeff,
+                else:
+                    mult = self.racineEvidente * res[-1]
+                    add = mult + coeff
+                    res += add,
+            print("Après un passage par le tableau, on trouve ces coefficients là: ", res)
+            self.polynomeFactorise += str('(x + ' + str(-int(self.racineEvidente)) + ')')
+            self.coeffsToEqua(res)
+
+        self.resolutionSecondDegre()
+        print(self.polynomeFactorise)
