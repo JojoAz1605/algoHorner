b097363cc66e984014e1459703286f2e2c96e9e6
JojoAz1605 <joris.masson2002@gmail.com>
2021-12-02 18:57:48 +0100

    Rajout des 3/4 des commentaires

--- src/Polynome.py
+++ src/Polynome.py
@@ -16,123 +16,136 @@ class Polynome:
         self.horner()
 
     def afficheMonomes(self):
+        """Affiche les monomes du polynôme"""
         print("Les monômes sont: ", end='')
-        for monome in self.monomes:
-            print(monome, end=', ')
+        for monome in self.monomes:  # pour tous les monomes
+            print(monome, end=', ')  # affiche le monome
         print()
 
     def affichePuissanceEtCoeff(self):
+        """Affiche le dico des puisances et des coeffs"""
         print("Les puissances et les coeffs: ", self.puissancesEtCoeffs)
 
     def trouvePuissancesEtCoeff(self):
-        for monome in self.monomes:
-            self.puissancesEtCoeffs[monome.puissance] = monome.coeff
-        self.affichePuissanceEtCoeff()
+        """Récupère les coeffs et les puissances de chaque monomes et les stocke dans un dico"""
+        for monome in self.monomes:  # pour tous les monomes
+            self.puissancesEtCoeffs[monome.puissance] = monome.coeff  # la puissance en clé, le coeff en valeur
+        self.affichePuissanceEtCoeff()  # affiche le dico
 
     def trouveMonomes(self):
         """Cherche les monômes de l'équation"""
-        separateurs = "+-"
-        aAjouter = ''
+        separateurs = "+-"  # ce qui défini le début ou la fin d'un monome
+        aAjouter = ''  # le monome à ajouter
 
-        for elem in self.equation:
-            if elem in separateurs:
-                if len(aAjouter) == 0:
-                    aAjouter += elem
-                elif aAjouter != '':
-                    self.monomes += Monome(aAjouter),
-                    aAjouter = elem
+        for elem in self.equation:  # pour toutes les lettres de l'équation
+            if elem in separateurs:  # si la lettre est un séparateur
+                if len(aAjouter) == 0:  # si le monome est vide
+                    aAjouter += elem  # ajoute le séparateur
+                elif aAjouter != '':  # si a ajouter n'est pas vide
+                    self.monomes += Monome(aAjouter),  # ajoute un objet Monome avec aAjouter comme équation
+                    aAjouter = elem  # pour le signe(peut-être)
             else:
-                aAjouter += elem
-        self.monomes += Monome(aAjouter),
-        self.afficheMonomes()
+                aAjouter += elem  # ajoute la lettre au monome à ajouter
+        self.monomes += Monome(aAjouter),  # ajoute le dernier monome récupérer(car pas de "séparateur" à la fin)
+        self.afficheMonomes()  # affiche les monomes
 
     def isSecondDegre(self):
-        for puissance in self.puissancesEtCoeffs.keys():
-            if puissance is not None and puissance > 2:
-                return False
-        return True
+        """Retourne si oui ou non l'équation gérée est du second degré
+        :return: True ou False
+        """
+        return self.trouveDegre() == 2  # si le degré est égal à 2
 
     def trouveDegre(self):
-        puissances = tuple(self.puissancesEtCoeffs)
-        res = puissances[0]
-        for elem in puissances:
+        """Renvoie le degré de l'équation gérée
+        :return: le degré de l'équation(int)
+        """
+        puissances = tuple(self.puissancesEtCoeffs.keys())  # liste des puissances
+        res = puissances[0]  # prend la première puissance qui vient
+        for elem in puissances:  # pour toutes les puissances
             try:
-                if int(elem) > res:
-                    res = int(elem)
-            except TypeError:
+                if int(elem) > res:  # si la puissance est supérieure à res(qui est censée être la plus grande)
+                    res = int(elem)  # remplace res par elem
+            except TypeError:  # pour gérer le None
                 pass
-        return res
+        return res  # retour du résultat
 
     def standardise(self):
-        degre = self.trouveDegre()
-        for i in range(degre, 1, -1):
-            if i not in self.puissancesEtCoeffs.keys():
-                self.puissancesEtCoeffs[i] = 0
-                print(i, "n'est pas présent, rajout.\nLe dico maintenant: ", self.puissancesEtCoeffs)
+        """Standardise l'équation"""
+        degre = self.trouveDegre()  # le degre de l'équation
+        for i in range(degre, 1, -1):  # parcours les puissances dans l'ordre décroissant
+            if i not in self.puissancesEtCoeffs.keys():  # si une puissance(qui est censée être présente) n'est pas présente
+                self.puissancesEtCoeffs[i] = 0  # l'ajoute et met son coeff à 0(puisque non présent de base)
+                print(i, "n'est pas présent, rajout.\nLe dico maintenant: ", self.puissancesEtCoeffs)  # affichage
 
     def trouveRacineEvidente(self):
         """Trouve une racine évidente"""
-        coeffs = tuple(self.puissancesEtCoeffs.values())
-        puissances = tuple(self.puissancesEtCoeffs.keys())
+        coeffs = tuple(self.puissancesEtCoeffs.values())  # les coeffs
+        puissances = tuple(self.puissancesEtCoeffs.keys())  # les puissances
 
-        equation = ''
-        for i in range(len(self.monomes)):
-            if puissances[i] is not None:
-                if coeffs[i] > 0:
-                    equation += '+' + str(coeffs[i]) + 'x^' + str(puissances[i])
+        equation = ''  # l'équation
+        for i in range(len(self.monomes)):  # boucle de 0 jusqu'à la longeur de la liste des monomes
+            if puissances[i] is not None:  # si la puissance i n'est pas None
+                if coeffs[i] > 0:  # si le coeff de i est supérieur à 0
+                    equation += '+' + str(coeffs[i]) + 'x^' + str(puissances[i])  # ajoute à l'équation(avec le '+' devant)
                 else:
-                    equation += str(coeffs[i]) + 'x^' + str(puissances[i])
+                    equation += str(coeffs[i]) + 'x^' + str(puissances[i])  # ajoute à l'équation(comme c'est < 0, le '-' esr de base
             else:
-                equation += str(coeffs[i])
-        for i in range(-1000, 1000):
-            equa = equation.replace('x', "*(" + str(i) + ')').replace('^', '**')
-            resEqua = eval(equa)
-            if resEqua == 0:
-                self.racineEvidente = i
-                break
-        print("La racine évidente trouvée est: ", self.racineEvidente)
+                equation += str(coeffs[i])  # sinon ajoute le coeff(car pas de puissance, juste un entier)
+
+        for i in range(-1000, 1000):  # boucle de -1000 jusqu'à 1000
+            equa = equation.replace('x', "*(" + str(i) + ')').replace('^', '**')  # remplace des éléments de l'équation par des symboles plus compréhensible par la focntion eval()
+            resEqua = eval(equa)  # résout l'équation
+            if resEqua == 0:  # si le résultat est 0
+                self.racineEvidente = i  # alors i est une racine évidente
+                break  # sort de la boucle
+        print("La racine évidente trouvée est: ", self.racineEvidente)  # affichage
 
     def resolutionSecondDegre(self):
-        abc = ()
-        print(self.puissancesEtCoeffs)
-        for i in range(2, -1, -1):
+        """Résout une équation du 2nd degré"""
+        abc = ()  # stocke les coeff a, b, et c
+        print(self.puissancesEtCoeffs)  # affichage
+        for i in range(2, -1, -1):  # le puissances(2, 1 et 0(None))
             try:
-                if i != 0:
-                    abc += self.puissancesEtCoeffs[i],
+                if i != 0:  # i i ne vaut pas 0
+                    abc += self.puissancesEtCoeffs[i],  # ajoute le coeff de i
                 else:
-                    abc += self.puissancesEtCoeffs[None],
-            except KeyError:
-                abc += 0,
+                    abc += self.puissancesEtCoeffs[None],  # ajoute l'entier associée à None
+            except KeyError:  # si la clé n'est pas présente
+                abc += 0,  # ajoute 0 aux coeff
+        # comme tout est censé être dans l'ordre
         a = abc[0]
         b = abc[1]
         c = abc[2]
-        delta = ((b ** 2) - (4 * a * c))
 
-        print("Delta vaut: ", delta)
+        delta = ((b ** 2) - (4 * a * c))  # calcule delta
 
-        if delta == 0:
-            x0 = -b / (2 * a)
-            self.polynomeFactorise += str(a) + '(x' + str(-x0) + ')'
-        elif delta < 0:
-            self.polynomeFactorise += '(' + self.equation + ')'
-        else:
-            x1 = (-b - (delta ** 0.5)) / (2 * a)
-            x2 = (-b + (delta ** 0.5)) / (2 * a)
-            self.polynomeFactorise += str(a) + '(x + ' + str(-x1) + ')' + '(x + ' + str(-x2) + ')'
+        print("Delta vaut: ", delta)  # affiche delta
+
+        if delta == 0:  # si delta vaut 0
+            x0 = -b / (2 * a)  # une racine
+            self.polynomeFactorise += str(a) + '(x' + str(-x0) + ')'  # ajoute le résultat au résultat final
+        elif delta < 0:  # si delta est < 0
+            self.polynomeFactorise += '(' + self.equation + ')'  # ajoute l'équation telle quelle(mais la met entre ())
+        else:  # delta > 0
+            x1 = (-b - (delta ** 0.5)) / (2 * a)  # 1ère racine
+            x2 = (-b + (delta ** 0.5)) / (2 * a)  # 2ème racine
+            self.polynomeFactorise += str(a) + '(x + ' + str(-x1) + ')' + '(x + ' + str(-x2) + ')'  # ajoute les résultat au résultat final
 
     def puissancesEtCoeffs2Equa(self):
-        equa = ''
-        for puissance in self.puissancesEtCoeffs:
-            if puissance is None:
-                if self.puissancesEtCoeffs[puissance] > 0:
-                    equa += '+' + str(self.puissancesEtCoeffs[None])
+        """Convertit le dico en une équation"""
+        equa = ''  # l'équation
+        for puissance in self.puissancesEtCoeffs:  # pour toutes les puissances
+            if puissance is None:  # i la puissance vaut None
+                if self.puissancesEtCoeffs[puissance] > 0:  # si le coeff est > à 0
+                    equa += '+' + str(self.puissancesEtCoeffs[None])  # rajoute +coeff_de_None
                 else:
-                    equa += str(self.puissancesEtCoeffs[None])
+                    equa += str(self.puissancesEtCoeffs[None])  # rajoute coeff_de_None
             else:
-                equa += str(self.puissancesEtCoeffs[puissance]) + 'x^' + str(puissance)
-        self.equation = equa
+                equa += str(self.puissancesEtCoeffs[puissance]) + 'x^' + str(puissance)  # rajoute le coeff x^puissance
+        self.equation = equa  # défini l'équation à celle qui vient d'être "calculée"
 
     def horner(self):
+        # TODO Commentaires
         while True:
             if self.equation != '':
                 print("L'équation est: ", self.equation)
